ef2a6f67a265558783535938d3a649ee4abf82c3
Trong-eng <whatdoyoumean6667@gmail.com>
2024-06-24 22:58:33 +0700

    Update solution

--- week3exerciseAIO2024/my_stack.py
+++ week3exerciseAIO2024/my_stack.py
@@ -1,21 +1,27 @@
 class MyStack:
-  def __init__(self, capicity):
-    self.capicity = capicity
-    self.stack = []
-  def is_empty(self):
-    return len(self.stack) == 0
-  def is_full(self):
-    return len(self.stack) == self.capicity
-  def push(self, item):
-    if self.is_full():
-      raise Exception('Overflow')
-    self.stack.append(item)
-  def pop(self):
-    if self.is_empty():
-      raise Exception('Underflow')
-    self.stack.pop()
-  def top(self):
-    if self.is_empty:
-      return 'is empty'
-    return self.stack[-1]
-  
+    def __init__(self, capacity):
+        self.__capacity = capacity
+        self.__stack = []
+
+    def is_empty(self):
+        return len(self.__stack) == 0
+
+    def is_full(self):
+        return len(self.__stack) == self.__capacity
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.__stack.pop()
+
+    def push(self, value):
+        if self.is_full():
+            return False
+        self.__stack.append(value)
+        return True
+
+    def top(self):
+        if self.is_empty():
+            return None
+        return self.__stack[-1]
+      
